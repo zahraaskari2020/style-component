a7e4f84dd499869c8a455f6e5ad636ecce45c492
DESKTOP-0LVN62V\Modern <askari.zahra94@gmail.com>
2024-06-16 12:36:50 -0500

    finish

--- style-component/src/App.jsx
+++ style-component/src/App.jsx
@@ -1,38 +1,46 @@
-import styled, { css } from 'styled-components'
-import Navbar from './components/Navbar';
-import Intro from './components/Intro';
-import Feature from './components/Feature';
-import Service from './components/Service';
-import Price from './components/Price';
+
+import styled, { css } from "styled-components";
+import Contact from "./components/Contact";
+import Feature from "./components/Feature";
+import Footer from "./components/Footer";
+import Intro from "./components/Intro";
+import Navbar from "./components/Navbar";
+import Price from "./components/Price";
+import Service from "./components/Service";
 
 const Container = styled.div`
   height: 100vh;
-  overflow:hidden;
-  position:relative;
-`
-const Shape = css`
-  width:100%;
-  height:100%;
-  position:absolute;
-  top:0;
-  left:0;
-  z-index:-1;
-`
-const IntroShape = styled.div`
-  ${Shape}
-  clip-path: polygon(60% 0%, 100% 0%, 100% 100%, 55% 100%);
-  background-color:crimson
+  overflow: hidden;
+  position: relative;
 `;
+
+const Shape = css`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -1;
+`;
+
+const IntoShape = styled.div`
+  ${Shape}
+  clip-path: polygon(67% 0, 100% 0%, 100% 100%, 55% 100%);
+  background-color: crimson;
+`;
+
 const FeatureShape = styled.div`
   ${Shape}
   clip-path: polygon(0 0, 55% 0%, 33% 100%, 0 100%);
-  background-color:pink
+  background-color: pink;
 `;
+
 const ServiceShape = styled.div`
   ${Shape}
   clip-path: polygon(0 0, 33% 0%, 33% 100%, 0 100%);
   background-color: #f88497;
 `;
+
 const PriceShape = styled.div`
   ${Shape}
   clip-path: polygon(33% 0, 100% 0%, 100% 100%, 67% 100%);
@@ -40,29 +48,32 @@ const PriceShape = styled.div`
 `;
 
 const App = () => {
+  const smallScreen = window.screen.width <= 480 ? true : false;
   return (
-  <>
-    <Container>
-      <Navbar />
-      <Intro />
-      <IntroShape />
-    </Container>
-    <Container>
-      <Feature />
-      <FeatureShape />
-    </Container>
-    <Container>
-      <Service />
-      <ServiceShape />
-    </Container>
-    <Container>
-      <Price />
-      <PriceShape />
-    </Container>
-  </>
-  
-  );  
-  
-}
+    <>
+      <Container>
+        <Navbar />
+        <Intro />
+        <IntoShape />
+      </Container>
+      <Container>
+        <Feature />
+        <FeatureShape />
+      </Container>
+      <Container>
+        <Service />
+        {!smallScreen && <ServiceShape />}
+      </Container>
+      <Container>
+        <Price />
+        <PriceShape />
+      </Container>
+      <Container>
+        <Contact />
+        <Footer />
+      </Container>
+    </>
+  );
+};
 
 export default App;
